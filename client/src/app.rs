de806549193716a5e3f7424e3cecd408c8846576
Justin Schaaf <justin@justinschaaf.com>
2024-12-02 00:20:34 -0800

    feat: start designing list ui

--- client/src/app.rs
+++ client/src/app.rs
@@ -1,25 +1,42 @@
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
+use egui::Layout;
+use egui::TextStyle::Button;
+use egui::WidgetType::SelectableLabel;
+use crate::app::Page::{Logs, Pigs, System, Users};
 
-    #[serde(skip)] // This how you opt-out of serialization of a field
-    value: f32,
+/// We derive Deserialize/Serialize so we can persist app state on shutdown.
+
+#[derive(Debug, PartialEq)]
+#[derive(serde::Deserialize, serde::Serialize)]
+enum Page {
+    Pigs,
+    Logs,
+    Users,
+    System,
 }
 
-impl Default for TemplateApp {
+#[derive(serde::Deserialize, serde::Serialize)]
+#[serde(default)] // if we add new fields, give them default values when deserializing old state
+pub struct PigWebClient {
+
+    // The currently open page, see above for options
+    page: Page,
+
+    // The current search query, opt out of serialization
+    #[serde(skip)]
+    query: String,
+
+}
+
+impl Default for PigWebClient {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            page: Pigs,
+            query: String::default(),
         }
     }
 }
 
-impl TemplateApp {
+impl PigWebClient {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
@@ -33,10 +50,49 @@ impl TemplateApp {
 
         Default::default()
     }
+
+    fn populate_menu(&mut self, ctx: &egui::Context, ui: &mut egui::Ui) {
+
+        ui.add_space(2.0);
+
+        egui::widgets::global_theme_preference_switch(ui);
+
+        ui.separator();
+
+        // TODO only show pages you have access to
+        ui.selectable_value(&mut self.page, Pigs, " 🐖 Pigs ");
+        ui.add_enabled(false, egui::SelectableLabel::new(false, " 📄 Logs "));
+        ui.add_enabled(false, egui::SelectableLabel::new(false, " 😐 Users "));
+        ui.add_enabled(false, egui::SelectableLabel::new(false, " ⛭ System "));
+
+        // Show debug warning
+        if cfg!(debug_assertions) {
+            ui.separator();
+            egui::warn_if_debug_build(ui);
+        }
+
+        // This right aligns it on the same row
+        let is_web = cfg!(target_arch = "wasm32");
+        ui.with_layout(Layout::right_to_left(egui::Align::RIGHT), |ui| {
+
+            // Show the quit button if somehow this gets run on desktop
+            // (you shouldn't, dumbass)
+            if !is_web && ui.button("🗙").clicked() {
+                ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+            }
+
+            // Logout
+            ui.button("⎆");
+
+        });
+
+    }
+
 }
 
-impl eframe::App for TemplateApp {
-    /// Called by the frame work to save state before shutdown.
+impl eframe::App for PigWebClient {
+
+    /// Called by the framework to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
@@ -47,63 +103,47 @@ impl eframe::App for TemplateApp {
         // For inspiration and more examples, go to https://emilk.github.io/egui
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-
             egui::menu::bar(ui, |ui| {
-                // NOTE: no File->Quit on web pages!
-                let is_web = cfg!(target_arch = "wasm32");
-                if !is_web {
-                    ui.menu_button("File", |ui| {
-                        if ui.button("Quit").clicked() {
-                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-                        }
-                    });
-                    ui.add_space(16.0);
-                }
-
-                egui::widgets::global_theme_preference_buttons(ui);
+                self.populate_menu(ctx, ui);
             });
+        });
+
+        egui::SidePanel::left("left_panel").show(ctx, |ui| {
+
+            // The central panel the region left after adding TopPanel's and SidePanel's
+
+            ui.add_space(8.0);
+            ui.heading("The Pig List");
+            ui.add_space(8.0);
+
+            ui.horizontal(|ui| {
+                ui.add(egui::TextEdit::singleline(&mut self.query).hint_text("Search"));
+                ui.button("+ Add");
+            });
+
+            ui.add_space(4.0);
+
+            egui::ScrollArea::vertical()
+                .auto_shrink(false)
+                .show(ui, |ui| {
+                    egui::Grid::new("pig_list")
+                        .num_columns(1)
+                        .striped(true)
+                        .show(ui, |ui| {
+                            for i in 0..1000 {
+                                ui.with_layout(Layout::top_down_justified(egui::Align::LEFT), |ui| {
+                                    ui.label(format!("This is line {i}"));
+                                });
+                                ui.end_row();
+                            }
+                        });
+                });
+
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
-
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
-
-            ui.separator();
-
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
-            });
+            ui.label("hi :3");
         });
-    }
-}
 
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
+    }
 }
