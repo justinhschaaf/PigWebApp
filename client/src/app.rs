5b831d68eae4a64729c9853340db910087a778bf
Justin Schaaf <justin@justinschaaf.com>
2024-12-03 03:39:20 -0800

    chore: configure automatic formatting
    remember to enable it upon saving it in rustrover

--- client/src/app.rs
+++ client/src/app.rs
@@ -1,12 +1,14 @@
 use crate::app::Page::{Logs, Pigs, System, Users};
 use egui::TextStyle::Button;
-use egui::{menu, widgets, Align, CentralPanel, Context, Label, Layout, SelectableLabel, Sense, SidePanel, TextEdit, TopBottomPanel, Ui, ViewportCommand, Widget};
+use egui::{
+    menu, widgets, Align, CentralPanel, Context, Label, Layout, SelectableLabel, Sense, SidePanel, TextEdit,
+    TopBottomPanel, Ui, ViewportCommand, Widget,
+};
 use egui_extras::Column;
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 
-#[derive(Debug, PartialEq)]
-#[derive(serde::Deserialize, serde::Serialize)]
+#[derive(Debug, PartialEq, serde::Deserialize, serde::Serialize)]
 enum Page {
     Pigs,
     Logs,
@@ -17,7 +19,6 @@ enum Page {
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct PigWebClient {
-
     // The currently open page, see above for options
     page: Page,
 
@@ -28,16 +29,11 @@ pub struct PigWebClient {
     // The currently selected row
     #[serde(skip)]
     row_selection: Option<usize>,
-
 }
 
 impl Default for PigWebClient {
     fn default() -> Self {
-        Self {
-            page: Pigs,
-            query: String::default(),
-            row_selection: None,
-        }
+        Self { page: Pigs, query: String::default(), row_selection: None }
     }
 }
 
@@ -57,7 +53,6 @@ impl PigWebClient {
     }
 
     fn populate_menu(&mut self, ctx: &Context, ui: &mut Ui) {
-
         ui.add_space(2.0);
 
         widgets::global_theme_preference_switch(ui);
@@ -79,7 +74,6 @@ impl PigWebClient {
         // This right aligns it on the same row
         let is_web = cfg!(target_arch = "wasm32");
         ui.with_layout(Layout::right_to_left(Align::RIGHT), |ui| {
-
             // Show the quit button if somehow this gets run on desktop
             // (you shouldn't, dumbass)
             if !is_web && ui.button("🗙").clicked() {
@@ -90,13 +84,10 @@ impl PigWebClient {
             if ui.button("⎆").clicked() {
                 println!("TODO"); // TODO implement me
             }
-
         });
-
     }
 
     fn populate_sidebar(&mut self, ctx: &Context, ui: &mut Ui) {
-
         ui.add_space(8.0);
         ui.heading("The Pig List");
         ui.add_space(8.0);
@@ -118,30 +109,23 @@ impl PigWebClient {
             .cell_layout(Layout::left_to_right(Align::Center))
             .body(|mut body| {
                 body.rows(18.0, 1000, |mut row| {
-
                     let i = row.index();
                     row.set_selected(self.row_selection.is_some() && self.row_selection.unwrap() == i);
 
                     // Make sure we can't select the text or else we can't click the row behind
                     row.col(|ui| {
-                        Label::new(format!("This is line {i}"))
-                            .selectable(false)
-                            .ui(ui);
+                        Label::new(format!("This is line {i}")).selectable(false).ui(ui);
                     });
 
                     if row.response().clicked() {
                         self.row_selection = Some(i);
                     }
-
                 });
             });
-
     }
-
 }
 
 impl eframe::App for PigWebClient {
-
     /// Called by the framework to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
@@ -165,6 +149,5 @@ impl eframe::App for PigWebClient {
         CentralPanel::default().show(ctx, |ui| {
             ui.label("hi :3");
         });
-
     }
 }
